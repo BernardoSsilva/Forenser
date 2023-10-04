b8fa1476bcd6f52fff0b3545522b482fa1a05711
BernardoSsilva <Bernardo.ssilva0808@gmail.com>
2023-10-04 09:18:59 -0300

    added routes

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './login.css';
+
+const Login = () => {
+    return(
+        <div>
+            <h1>Registrar-se</h1>
+            <p>Nome de usuario</p>
+            <input type="text" name="user" id="" />
+            <p>Senha </p>
+            <input type="password" name="" id="" />
+        </div>
+    )
+}
+
+export default Login
