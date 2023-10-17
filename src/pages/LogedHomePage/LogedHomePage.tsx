b0bbf27a4dd4024116ff5f0a00c1b422d3af16ae
BernardoSsilva <Bernardo.ssilva0808@gmail.com>
2023-10-17 15:03:35 -0300

    created the main screen

--- src/pages/LogedHomePage/LogedHomePage.tsx
+++ src/pages/LogedHomePage/LogedHomePage.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import './LogedHomePage.css';
 
 const LogedHomePage = () => {
     return(
-        <div>
-            funciona
+        <div className ="container">
+            <header className='header'>
+                <h1>
+                    Bem-vindo
+                </h1>
+            </header>
+            <nav className='sideMenu'>
+                <ul className='list'>
+                    <li className='listField'>
+                        <a href="#">Boletim de ocorrencia</a>
+                    </li>
+
+                    <li className='listField'>
+                        <a href="#">Reconhecimento facial</a>
+                    </li>
+
+                    <li className='listField'>
+                        <a href="#">Sobre nós</a>
+                    </li>
+
+                    <li className='listField'>
+                        <a href="#">como utilizar</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
--- src/routes/RoutesDoc.tsx
+++ src/routes/RoutesDoc.tsx
@@ -5,11 +5,12 @@ import Login from "../pages/Login/Login";
 import Register from "../pages/register/Register";
 import LogedHomePage from "../pages/LogedHomePage/LogedHomePage";
 
+
 const RoutesDoc = () => {
     return(
         <BrowserRouter>
             <Routes>
-                <Route element={ <Home /> } path="/"/>
+                <Route element={ <Home />} path="/"/>
                 <Route element={ <Login /> } path="/login" />
                 <Route element = { <Register /> } path="/register" />
                 <Route element={<LogedHomePage/>} path="/sesstrue"/>
