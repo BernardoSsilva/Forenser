cc6ed746e1812f1fe2116db5e4c84a341f99be9a
BernardoSsilva <Bernardo.ssilva0808@gmail.com>
2023-10-18 16:16:23 -0300

    .

--- src/pages/LogedHomePage/LogedHomePage.tsx
+++ src/pages/LogedHomePage/LogedHomePage.tsx
@@ -1,32 +1,14 @@
-import React, { useEffect, useState } from 'react';
 import './LogedHomePage.css';
-import jwt from 'jsonwebtoken';
 
 
 const LogedHomePage = () => {
 
-    const [nomeUsuario, setNomeUsuario] = useState('');
-
-    useEffect(() => {
-        // Obtenha o token do localStorage
-        const token = localStorage.getItem('jwtToken');
-
-        if (token) {
-            // Decodifique o token para acessar o payload
-            const decodedToken = jwt.decode(token);
-
-            if (decodedToken && decodedToken.nome) {
-                // Extraia o nome do payload
-                setNomeUsuario(decodedToken.nome);
-            }
-        }
-    }, []);
 
     return(
         <div className ="container">
             <header className='header'>
                 <h1>
-                    Bem-vindo {nomeUsuario}
+                    Bem-vindo 
                 </h1>
             </header>
             <nav className='sideMenu'>
